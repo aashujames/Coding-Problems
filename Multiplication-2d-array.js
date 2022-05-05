c738a1e5e8e1f2164f214131974acb01dabae908
Ashish Gupta <ashishgupt25@gmail.com>
2022-05-05 16:12:34 +0530

    Final multiplicaton 2d array

--- Multiplication-2d-array.js
+++ Multiplication-2d-array.js
@@ -2,27 +2,28 @@ class Solution {
     multiplymatrix(A, B) {
         let AB = [];
 
-        for (let i = 0; i < A.length; i++) {
-            let count = 0;
-            let k = 0;
-            let eleAB = 0;
-            while (count < B[0].length) {
-                for (let j = 0; j < A[0].length; j++) {
-                    let eleA = A[i][j];
+        if (A[0].length === B.length) {
+            for (let i = 0; i < A.length; i++) {
+                AB.push([]);
+                let count = 0;
+                let k = 0;
+                let eleAB = 0;
+                while (count < B[0].length) {
+                    for (let j = 0; j < A[0].length; j++) {
+                        let eleA = A[i][j];
+                        let eleB = B[j][k];
+                        eleAB += eleA * eleB;
+                    }
+                    AB[i].push(eleAB);
 
-                    let eleB = B[j][k];
-                    console.log(eleA);
-                    console.log(eleB);
-                    eleAB += eleA * eleB;
-
-                    console.log(eleAB);
+                    count++;
+                    k++;
+                    eleAB = 0;
                 }
-                AB.push([eleAB]);
-                console.log(AB);
-                count++;
-                k++;
-                eleAB = 0;
             }
+            return AB;
+        } else {
+            return "Cannot be multiplied, Check condition!!";
         }
     }
 }
@@ -34,9 +35,8 @@ const ans = multiplymatrix.multiplymatrix(
         [4, 5, 6]
     ],
     [
-        [0, 4],
-        [3, 3],
-        [2, 1]
+        [0, 1],
+        [3, 0]
     ]
 );
 console.log(ans);
