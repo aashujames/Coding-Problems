22e5283cf6afd21a4811d273f1811e21fe2c233e
Ashish Gupta <ashishgupt25@gmail.com>
2023-04-28 15:41:09 +0530

    linked list cycle

---
+++ 2023/LLCycle.js
@@ -0,0 +1,15 @@
+//Floyd's Tortoise and Hare Algorithm
+var hasCycle = function (head) {
+    let fast = head,
+        slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast === slow) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(hasCycle([3, 4, 1, 8], 0));
